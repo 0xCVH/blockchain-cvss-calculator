5201ee46c3617fba6ce5c03a181d8a89dca49bf9
Michael Henriksen <mhenriksen@gitlab.com>
2021-02-12 14:46:35 +0100

    Disable Vue production replacement for now

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-vuedev='<script src="https://cdn\.jsdelivr\.net/npm/vue@2/dist/vue.js"></script>'
+vuedev='<script src="https://cdn\.jsdelivr\.net/npm/vue@2\.6\.12/dist/vue.js"></script>'
 vueprod='<script src="https://cdn\.jsdelivr\.net/npm/vue@2\.6\.12/dist/vue\.min\.js" integrity="sha384-cwVe6U8Tq7F/3JIj6xeDzOwuqeChcmRcdYqDGfoYmdAurw7L3f4dFHhEJKfxv96A" crossorigin="anonymous"></script>'
 
 echo " + Copying CSS files to public/"
@@ -10,11 +10,11 @@ echo " + Copying JavasCript files to public/"
 cp -R js public/
 echo " + Copying index.html to public/"
 cp index.html public/
-echo " + Replacing development Vue script with production version"
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' -e "s|${vuedev}|${vueprod}|g" public/index.html
-else
-  sed -i -e "s|${vuedev}|${vueprod}|g" public/index.html
-fi
+# echo " + Replacing development Vue script with production version"
+# if [[ "$OSTYPE" == "darwin"* ]]; then
+#   sed -i '' -e "s|${vuedev}|${vueprod}|g" public/index.html
+# else
+#   sed -i -e "s|${vuedev}|${vueprod}|g" public/index.html
+# fi
 
 echo " + Done!"
